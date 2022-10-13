0975e66a6e895174c7896d0bb5b901996d564100
Daniel Kronovet <kronovet@gmail.com>
2022-10-13 16:27:24 -0700

    Update smoke tests

--- test-smoke/colony-storage-consistent.js
+++ test-smoke/colony-storage-consistent.js
@@ -149,8 +149,8 @@ contract("Contract Storage", (accounts) => {
       console.log("miningCycleStateHash:", miningCycleStateHash);
       console.log("tokenLockingStateHash:", tokenLockingStateHash);
 
-      expect(colonyNetworkStateHash).to.equal("0x69b4c475be693967a884c8384862030426fca3942529657428b9b3d17304d867");
-      expect(colonyStateHash).to.equal("0x209ca5da64157f5060f76bd0854c8a22977703a05d29a3a0e1707902cb8942f2");
+      expect(colonyNetworkStateHash).to.equal("0x25e8213be3dc0269def8b888999814007d5c2b7c2b8a6600d425e0453076e920");
+      expect(colonyStateHash).to.equal("0x97b9839b665bdb38f9875182479694f0783529c36793ff18a58390efdb42c513");
       expect(metaColonyStateHash).to.equal("0xff23657f917385e6a94f328907443fef625f08b8b3224e065a53b690f91be0bb");
       expect(miningCycleStateHash).to.equal("0x264d4a83e21fef92f687f9fabacae9370966b0b30ebc15307653c4c3d33a0035");
       expect(tokenLockingStateHash).to.equal("0x983a56a52582ce548e98659e15a9baa5387886fcb0ac1185dbd746dfabf00338");
